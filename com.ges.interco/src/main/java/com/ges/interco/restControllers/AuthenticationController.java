c827d4ba63df19363b9555bc926211547377b067
Anas <tariq.tariq.anas@gmail.com>
2020-01-09 14:56:11 +0100

    Adding Devtools ( + none important stuff in AuthController )

--- com.ges.interco/src/main/java/com/ges/interco/restControllers/AuthenticationController.java
+++ com.ges.interco/src/main/java/com/ges/interco/restControllers/AuthenticationController.java
@@ -1,18 +1,13 @@
 package com.ges.interco.restControllers;
 
-import java.security.Principal;
-import java.util.Base64;
-import java.util.List;
 import java.util.Optional;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ges.interco.dao.UsersRepository;
@@ -24,15 +19,20 @@ public class AuthenticationController {
 
 	@Autowired
 	UsersRepository user;
-	
 
 	@GetMapping("/auth")
-	public Optional<User> login(Principal principale) {
-		
+	public JSONObject login(Authentication connectedUser) {
+
 		Logger log = Logger.getLogger("Authentication Logger");
-		log.log(Level.INFO, "The Authenticated user is :" + principale.getName());
-		// For Now..
-		return user.findById(1L);
+		Optional<User> utilisateur = user.findByuserName(connectedUser.getName());
+
+		JSONObject json = new JSONObject();
+
+		utilisateur.get().getRoles().forEach(_w -> {
+			json.put("role", _w);
+		});
+
+		return json;
 	}
 
 }
